d342d2d21b1f2f6a0d56f145a2f2b2131d6275d3
GauharFatima <108114370+GauharFatima@users.noreply.github.com>
2024-03-06 16:13:10 +0530

    Update urls.py
    removed the login from account url

--- AccountsApp/urls.py
+++ AccountsApp/urls.py
@@ -3,7 +3,7 @@ from AccountsApp import views
 
 urlpatterns = [
     path('signup/', views.createUser, name='signup'),
-    path('login/', views.existingUser, name='login'),
+    path('', views.existingUser, name='login'),
     # path('feeds/', views.feedsView, name='feeds'),
     path('logout/', views.logoutUser, name='logout')
 ]
